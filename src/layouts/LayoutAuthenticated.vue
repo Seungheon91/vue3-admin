cedc405117000d971524d1a6d44becc518fa448f
StarkLSH <lsh@zipdoc.kr>
2023-03-24 17:30:41 +0900

    feat: layout 구성

---
+++ src/layouts/LayoutAuthenticated.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import NavBar from '@/layouts/NavBar.vue'
+</script>
+
+<template>
+  <div :class="{}">
+    <div>
+      <NavBar />
+    </div>
+  </div>
+</template>
---
+++ src/layouts/NavBar.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import { containerMaxW } from '@/config.js'
+
+defineProps({
+  menu: {
+    type: Array,
+    required: true
+  }
+})
+
+const emit = defineEmits(['menu-click'])
+
+const menuClick = (event, item) => {
+  emit('menu-click', event, item)
+}
+
+const isMenuNavBarActive = ref(false)
+</script>
+<template>
+  <nav
+    class="top-0 inset-x-0 fixed bg-gray-50 h-14 z-30 transition-position w-screen lg:w-auto dark:bg-slate-800"
+  >
+    <div class="flex lg:items-stretch" :class="containerMaxW">
+      <div></div>
+    </div>
+  </nav>
+</template>
